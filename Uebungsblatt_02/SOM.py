d8ae476c57c97a0374c90a36ab09d8659ff66b86
KingMus <marcofm47@gmail.com>
2018-04-11 10:09:03 +0200

    added Uebungsblatt02

---
+++ Uebungsblatt_02/SOM.py
@@ -0,0 +1,19 @@
+"""Class for a own implementation of a self-organizing map (SOM).
+
+Description
+-----------
+...
+
+Names of the students
+---------------------
+1. 
+2.
+
+"""
+import numpy as np
+
+class SOM:
+    """Self-organizing map implementation"""
+    
+    def __init__(self):
+        pass
