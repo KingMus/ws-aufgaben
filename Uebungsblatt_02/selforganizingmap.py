7b10911d117de0d7666b318b59017807a78216be
KingMus <bomberurs+it@gmail.com>
2018-04-20 12:41:44 +0200

    added som functions for competition

--- Uebungsblatt_02/selforganizingmap.py
+++ Uebungsblatt_02/selforganizingmap.py
@@ -12,6 +12,7 @@ Names of the students
 """
 import numpy as np
 import pandas as pd
+import random
 
 class SOM:
     """Self-organizing map implementation"""
@@ -23,12 +24,71 @@ class SOM:
         self.somRows = somRows
         self.somColumns = somColumns
 
-        #self.map = self.generateRandomMap()
+        self.map = self.generateRandomMap()
 
         print(df.info())
         print(label[0])
         print(somRows)
         print(somColumns)
+
+        print(self.__str__())
+
+
+    def generateRandomMap(self):
+
+        #shape describes tupel of dimensions
+        nFeatures = self.df.count().shape[0]
+
+        #zeros initializes random map
+        map = np.zeros([self.somRows, self.somColumns, nFeatures], dtype=float)
+
+        for row in range(self.somRows):
+            for col in range(self.somColumns):
+                for feature in range(nFeatures):
+
+                    #find random value between min and max value in dataframe
+                    minValueInDf = np.nanmin(self.df.iloc[:, feature].values)
+                    maxValueInDf = np.nanmax(self.df.iloc[:, feature].values)
+                    neuronFeature = np.random.uniform(minValueInDf, maxValueInDf, 1)
+                    map[row][col][feature] = neuronFeature
+
+        return map
+
+    def getRandomValue(self);
+
+        return random.choice(self.df)
+
+    #Competition
+    def findBestMatchingUnit(self);
+
+        bmu = self.map[0][0] #set an inital bmu
+        bmuRowI = 0
+        bmuColI = 0
+        bmuDistance = float("inf")  #max float number to have highest possible distance
+
+        for row in range(self.somRows):
+            for col in range(self.somColumns):
+
+                dist = np.linalg.norm(self.getRandomValue() - self.map[row][col])
+                if (dist < bmuDistance):
+                    bmu = self.map[row][col]
+                    bmuRowI = row
+                    bmuColI = col
+                    bmuDistance = dist
+
+        print("sample:{0}, bmu:{1}\n".format(self.getRandomValue(), bmu))
+        
+        return [bmuRowI,bmuColI]
+
+
+    #Adaption
+    def adjustmentOfWeights(self);
+
+
+        weightNow =
+
+
+        return weightAfter
 
 
         pass
